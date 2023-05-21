4ee44c804d9376876729eab504f1a78813b16daa
kotireddy1258 <koti1258@gmail.com>
2023-05-21 12:53:28 +0530

    source script example

---
+++ script13.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#vi env.sh
+#FNAME=KOTIREDDY
+#LNAME=BADAM
+#DOB=16-07-1987
+#ADDR=HYDERABAD\ in\ KONDAPUR
+#WORK=LINUX\ SERVER\ ADMIN
+source env.sh
+echo ""
+echo "Hi GOOD MORNING"
+echo ""
+sleep 2
+echo "My Fist Name Is =$FNAME"
+echo ""
+sleep 2
+echo "My Last Name Is =$LNAME"
+echo ""
+sleep 2
+echo "My Date Of Birth is =$DOB"
+echo ""
+sleep 2
+echo "I am present in address =$ADDR"
+echo ""
+sleep 2
+echo "Present i am work role is =$WORK"
+echo ""
+echo ""
