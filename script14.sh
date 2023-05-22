b41043c05fc4e493998dba0938062c977ecc7675
kotireddy1258 <koti1258@gmail.com>
2023-05-22 11:29:53 +0530

    gitscript  update

--- script14.sh
+++ script14.sh
@@ -4,11 +4,24 @@ git add $1
 
 echo "git file added successfuly"
 
+echo "******************************************"
+echo ""
+sleep 3
+
 git commit -m "$2  update"
+
+echo "******************************************"
+echo ""
+sleep 3
 
 echo "git commit successfuly"
 
+echo "******************************************"
+echo ""
+sleep 3
 git push -u origin main
 
 echo "git file push main successfuly"
 
+echo "******************************************"
+echo ""
