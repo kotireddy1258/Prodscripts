e82f7614ce8a1f9e814144859d2eda50ff2587ad
kotireddy1258 <koti1258@gmail.com>
2023-05-26 13:24:25 +0530

    file  update

---
+++ gitscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "All Scripts"
+ls
+sleep 3
+echo "******************************"
+git add $1
+if [ $? -eq 0 ];then
+echo "git file added successfuly"
+else
+echo "git file not found"
+echo "******************************"
+fi
+echo "******************************"
+git commit -m "$2  update"
+if [ $? -eq 0 ];then
+echo "git file update successfuly"
+else
+echo "git file not update"
+echo "******************************"
+fi
+echo "******************************"
+git push -u origin main
+if [ $? -eq 0 ];then
+echo "git file push main successfuly"
+else
+echo "git file not pushed got error"
+fi
+echo "******************************"
+
